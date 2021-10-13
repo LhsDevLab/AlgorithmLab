0db63239d04abe405155c631abe2c1a524054acb
LeeHanSeong <jkhk7788@gmail.com>
2021-10-13 20:28:52 +0900

    Create 11947.py

---
+++ Baekjoon/problem/python/11947.py
@@ -0,0 +1,22 @@
+import sys;
+
+T = int(sys.stdin.readline());
+
+def F(n):
+    res = 10;
+    while res <= n:
+        res *= 10;
+    return res-1-n;
+
+cases = []
+for idx in range(T):
+    cases.append(int(sys.stdin.readline()))
+
+for case in cases:
+    target = 10;
+    while target <= case:
+        target *= 10;
+    target /= 2
+    if case < target:
+        target = case
+    print(int(target*F(target)))
