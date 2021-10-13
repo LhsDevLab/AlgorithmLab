c1fbbf921b0082af8421698ad0ee18f326862bb0
LeeHanSeong <jkhk7788@gmail.com>
2021-10-13 18:33:09 +0900

    Update 11947.js

--- Baekjoon/problem/js/unsolved/11947.js
+++ Baekjoon/problem/js/unsolved/11947.js
@@ -1,22 +1,29 @@
-let fs = require('fs');
-let input = fs.readFileSync('input.txt').toString().split("\n");
-//let input = fs.readFileSync('/dev/stdin').toString().split("\n");
+const readline = require('readline');
 
-let k = parseInt(input[0]);
-let cases = input.slice(1,k+1).map(e=>{return parseInt(e)});
-
-function F(n){
-    let res = 10;
-    while(res <= n)
-        res *= 10;
-    return res-1-n;
-}
-
-cases.forEach(e=>{
-    let target = 10;
-    while(target <= e)
-        target *= 10;
-    target /= 2;
-    if (e < target) target = e;
-    console.log(target*F(target));
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+let input = [];
+rl.on('line', (line)=>{
+    input.push(parseInt(line));
+    if (input.length == input[0]+1){
+        rl.close();
+    }
+}).on('close', ()=>{
+    function F(n){
+        let res = 10;
+        while(res <= n)
+            res *= 10;
+        return res-1-n;
+    }
+    input.shift();
+    input.forEach(e=>{
+        let target = 10;
+        while(target <= e)
+            target *= 10;
+        target /= 2;
+        if (e < target) target = e;
+        console.log(target*F(target));
+    });    
 });
