a985f51900ab5304d9397c25ecd30c5ba293f7fa
LeeHanSeong <jkhk7788@gmail.com>
2021-10-21 16:59:06 +0900

    update

---
+++ Baekjoon/problem/java/10818.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        String[] temp = br.readLine().split(" ");
+        Double min = Double.POSITIVE_INFINITY;
+        Double max = Double.NEGATIVE_INFINITY;
+        for(String e : temp){
+            Double num = Double.parseDouble(e);
+            if (min > num) min = num;
+            if (max < num) max = num;
+        }
+        System.out.printf("%d %d\n",(int)Math.floor(min), (int)Math.floor(max));
+    }
+}
