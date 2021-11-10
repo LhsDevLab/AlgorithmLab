4932d349df65b34b255c184902c9892b6cb3b42f
LeeHanSeong <jkhk7788@gmail.com>
2021-11-10 12:28:35 +0900

    Update 11660.py

--- Baekjoon/problem/python/11660.py
+++ Baekjoon/problem/python/11660.py
@@ -1,19 +1,14 @@
 import sys
 
 N,M = map(int,sys.stdin.readline().split(' '))
-preSumList = [[] for _ in range(N)]
-preSum2Arr = [[0 for _ in range(N+1)] for _ in range(N+1)]
-for i in range(N):
-    total = 0
-    preSumList[i].append(0)
-    for item in map(int,sys.stdin.readline().split(' ')):
-        total += item
-        preSumList[i].append(total)
+preSumList = [[0 for _ in range(N+1)] for _ in range(N+1)]
 
 for i in range(1,N+1):
-    for j in range(N+1):
-        preSum2Arr[i][j] = preSum2Arr[i-1][j] + preSumList[i-1][j]
-
+    input = tuple(map(int,sys.stdin.readline().split(' ')))
+    for j in range(1,N+1):
+        preSumList[i][j] = preSumList[i-1][j]+preSumList[i][j-1]-preSumList[i-1][j-1]+input[j-1]
+res = ""
 for _ in range(M):
     ay,ax,by,bx = map(int,sys.stdin.readline().split(' '))
-    print(preSum2Arr[by][bx] - preSum2Arr[ay-1][bx] - preSum2Arr[by][ax-1] + preSum2Arr[ay-1][ax-1])
+    res += str(preSumList[by][bx] - preSumList[ay-1][bx] - preSumList[by][ax-1] + preSumList[ay-1][ax-1])+'\n'
+print(res,end='')
