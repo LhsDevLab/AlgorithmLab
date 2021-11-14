7c290f86339a9cf28548533550f224f3825513e9
LeeHanSeong <jkhk7788@gmail.com>
2021-11-15 02:13:05 +0900

    Update 9007.py

--- Baekjoon/unsolved/9007.py
+++ Baekjoon/unsolved/9007.py
@@ -1,50 +1,29 @@
 import sys
 T = int(sys.stdin.readline())
 
-def addArrays(A,B):
+def mergeArrays(A,B):
     res = set()
     for a in A:
         for b in B:
             res.add(a+b)
     return list(res)
 
-def compare(arr,target):
-    arr.sort();
-    res = arr[0]
-    for e in arr:
-        if abs(target-e) < abs(target-res):
-            res = e
-    return res
-
-def binarySearch(arr,target):
-    f,r = 0,len(arr)-1
-    while True:
-        if r-f <= 1:
-            break;
-        mid = int((r+f)/2)
-        if arr[mid] > target:
-            r = mid
-        elif arr[mid] < target:
-            f = mid
-        else:
-            return mid,mid
-    return f,r
-    
 def solution(target, size):
     arr = []
     for _ in range(4):
         arr.append(list(map(int,sys.stdin.readline().split(' '))))
-    arr = [addArrays(arr[0],arr[1]),addArrays(arr[2],arr[3])]
-    if len(arr[0]) > len(arr[1]):
-        arr[0],arr[1] = arr[1],arr[0]
-    arr[1].sort()
-    res = arr[0][0]+arr[1][0]
-    for e in arr[0]:
-        a,b = binarySearch(arr[1],target-e)
-        a = arr[1][a]+e
-        b = arr[1][b]+e
-        res = compare([a,b,res], target)
-    return res
+    arr = [mergeArrays(arr[0],arr[1]),mergeArrays(arr[2],arr[3])]
+    arr[0].sort();arr[1].sort(reverse=True)
+    idx = 0
+    res = float("inf")
+    for A in arr[0]:
+        pre = (A+arr[1][idx])-target
+        while idx+1 < len(arr[1]) and abs(A+arr[1][idx+1]-target) <= abs(pre):
+            idx += 1
+            pre = (A+arr[1][idx])-target
+        if (abs(res) > abs(pre)) or (abs(res) == abs(pre) and pre < 0):
+            res = pre
+    return res+target
 for _ in range(T):
     target , size = map(int,sys.stdin.readline().split(' '))
     print(solution(target , size))
