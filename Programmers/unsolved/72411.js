6b34dc1ab10d627584e6cc43fc2442e4785672a7
LeeHanSeong <jkhk7788@gmail.com>
2021-11-19 19:47:59 +0900

    Update 72411.js

--- Programmers/unsolved/72411.js
+++ Programmers/unsolved/72411.js
@@ -1,15 +1,20 @@
 function solution(orders, course) {
-    let count = {a:10}
-    count.addToObj=(key)=>{
-        if (this[key] == undefined)
-            this[key] = 1
-        else
-            this[key] += 1
+    orders = orders.map(e=>new Set(e))
+    const union = (a,b)=>{
+        for (e of b)
+            a.add(e);
     }
-    for(let o of orders){
-        count.addToObj(o)
+    const getCourse = (size)=>{
+        let targets = orders.filter(e => e.size >= size)
+        let menus = new Set()
+        for (order of targets)
+            union(menus,order)
+        console.log(targets)
+        console.log(menus)
     }
-    console.log(count)
+
+    for (let e of course)
+        getCourse(e)
     return null
 }
 console.log(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"],[2,3,4]))
