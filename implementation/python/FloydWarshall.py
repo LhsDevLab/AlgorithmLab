07463d5130ccccd8516d14783ce0b897dc02eb9d
LeeHanSeong7 <54903040+LeeHanSeong7@users.noreply.github.com>
2021-12-27 23:09:51 +0900

    update

---
+++ implementation/python/FloydWarshall.py
@@ -0,0 +1,33 @@
+def F_W(edges):
+    N = len(edges);
+    dp = [];
+    for e in edges:
+        dp.append(e[:]);
+    for _ in range(N-1):
+        isDirty = False;
+        for node in range(1,N):
+            for i in range(1,N):
+                if i == node: continue;
+                for j in range(1,N):
+                    if j == node: continue;
+                    elif dp[i][j] > dp[i][node] + dp[node][j]:
+                        dp[i][j] = dp[i][node] + dp[node][j]
+                        isDirty = True
+        if isDirty == False:
+            break;
+    else:
+        return True;
+
+    return dp;
+
+N,M,W = map(int, input().split());
+edges = [[float("inf") for _ in range(N+1)] for _ in range(N+1)];
+for i in range(N+1):
+    edges[i][i] = 0;
+for _ in range(M):
+    S,E,T = map(int, input().split());
+    if edges[S][E] > T:
+        edges[S][E] = T;
+    if edges[E][S] > T:
+        edges[E][S] = T;
+F_W(edges);#isCycle : True
