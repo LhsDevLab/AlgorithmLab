03d57d99280c0d0eb6660b372945676423a431fc
LeeHanSeong7 <54903040+LeeHanSeong7@users.noreply.github.com>
2021-12-31 20:02:28 +0900

    Create 1654.py

---
+++ Baekjoon/unsolved/1654.py
@@ -0,0 +1,25 @@
+K,N = map(int, input().split());
+arr = [];
+for _ in range(K):
+    arr.append(int(input()));
+Max = 0;
+for e in arr:
+    Max += e;
+Max = int(Max/N);
+
+def count(l):
+    global arr;
+    total = 0;
+    for line in arr:
+        total += int(line/l);
+    return total;
+
+front = 1;
+rear = Max;
+while front < rear:
+    middle = int((front+rear)/2);
+    if count(middle) < N:
+        rear = middle;
+    else:
+        front = middle+1;
+print(rear-1);
