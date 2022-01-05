bc9980bc9393d67899c0a2a2a156ba7df9b1952a
LHS <jkhk7788@gmail.com>
2022-01-06 05:34:31 +0900

    update

---
+++ Baekjoon/unsolved/2775.py
@@ -0,0 +1,9 @@
+def sol(K,N):
+    arr = [i for i in range(1,N+1)];
+    for _ in range(K):
+        for i in range(N-1):
+            arr[i+1] += arr[i];
+    print(arr[N-1]);
+for _ in range(int(input())):
+    sol(int(input()),int(input()));
+    
