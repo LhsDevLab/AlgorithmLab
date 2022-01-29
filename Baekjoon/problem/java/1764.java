dbfa06aa7c19be555654a5b4f58a0dd0240cc4db
LeeHanSeong7 <54903040+LeeHanSeong7@users.noreply.github.com>
2022-01-29 13:33:52 +0900

    update

--- Baekjoon/problem/java/1764.java
+++ Baekjoon/problem/java/1764.java
@@ -10,18 +10,18 @@ class Main {
         String[] input = br.readLine().split(" ");
         int N = Integer.parseInt(input[0]);
         int M = Integer.parseInt(input[1]);
-        HashSet<String> set = new HashSet<String>();
+        HashSet<String> heard = new HashSet<String>();
         for (int i = 0; i < N; i++)
-            set.add(br.readLine());
-        ArrayList<String> called = new ArrayList<String>();
+            heard.add(br.readLine());
+        ArrayList<String> saw = new ArrayList<String>();
         for (int i = 0; i < M; i++) {
             String name = br.readLine();
-            if (set.contains(name))
-                called.add(name);
+            if (heard.contains(name))
+                saw.add(name);
         }
-        String[] arr = called.toArray(new String[0]);
+        String[] arr = saw.toArray(new String[0]);
         Arrays.sort(arr);
-        System.out.println(M - called.size());
+        System.out.println(saw.size());
         for (String name : arr)
             System.out.println(name);
     }
