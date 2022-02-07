ffa84e12b94c297096df47b16bac39718d5aa9e9
LeeHanSeong7 <54903040+LeeHanSeong7@users.noreply.github.com>
2022-02-07 14:02:42 +0900

    Update 42861.js

--- Programmers/42861.js
+++ Programmers/42861.js
@@ -1,19 +1,13 @@
 function solution(n, costs) {
     costs.sort((a,b)=>b[2]-a[2]);
-    stack = [];
-    while (stack.length != 0){
-        used = stack.pop();
-        if (used.length == n-1){
-            console.log(used)
-        }
-        for (let i=used.length; i<costs.length-(used.length-n); i++){
-            stack.push([...used,i]);
-        }
-    }
-    function combination(a,remain,n){
+    let indexs = combination(costs.length, n-1);
+    for (let list of indexs){
+        let set = new Set();
+        for (let i of list){
 
+        }
     }
-    combination([],[],n-1)
+    return indexs;
 }
 console.log(
     solution(4,[[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]])
