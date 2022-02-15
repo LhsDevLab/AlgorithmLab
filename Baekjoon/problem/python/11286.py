1722bfcabff16613045ae8edfae36eae601967e1
LeeHanSeong7 <54903040+LeeHanSeong7@users.noreply.github.com>
2022-02-15 13:38:35 +0900

    u

--- Baekjoon/problem/python/11286.py
+++ Baekjoon/problem/python/11286.py
@@ -5,6 +5,6 @@ queue = [];
 for _ in range(int(sys.stdin.readline())):
     n = int(sys.stdin.readline());
     if n == 0:
-        print(0 if len(queue) == 0 else heapq.heappop(queue)[1],end="!\n");
+        print(0 if len(queue) == 0 else heapq.heappop(queue)[1]);
     else:
         heapq.heappush(queue,(abs(n),n));
