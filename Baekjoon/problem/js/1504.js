61a7f1a0addfd34f469a9a51ee8296a990ec6af1
LeeHanSeong7 <54903040+LeeHanSeong7@users.noreply.github.com>
2022-03-02 16:46:31 +0900

    u

--- Baekjoon/problem/js/1504.js
+++ Baekjoon/problem/js/1504.js
@@ -1,8 +1,9 @@
 const reader = {
     input : require('fs').readFileSync('input.txt').toString().split("\r\n"),
     // input : require('fs').readFileSync('/dev/stdin').toString().split("\n"),
+    index : 0,
     read : function(){
-        return this.input.shift().split(" ").map(e=>parseInt(e));
+        return this.input[this.index++].split(" ").map(e=>parseInt(e));
     }
 }
 const [N,E] = reader.read();
@@ -16,16 +17,16 @@ function Dijkstra(start, graph){
     const size = graph.length;
     let res = Array.from({length:size},()=>Infinity);
     res[start] = 0;
-    let queue = [start];
-    while (queue.length != 0){
-        let node = queue.shift();
-        for (let i=0; i < size; i++){
-            let temp = graph[node][i]+res[node];
-            if (res[i] > temp){
-                res[i] = temp;
-                queue.push(i);
-            }
-        }
+    let remain = new Set(Array.from({length:size},(_,i)=>i));
+    while(remain.size != 0){
+        let temp = Array.from(remain);
+        let minNode = temp.reduce((a,c)=>{
+            return res[a] > res[c] ? c : a;
+        },temp.pop());
+        remain.delete(minNode);
+        temp = graph[minNode];
+        for (let i in res)
+            res[i] = Math.min(res[i], temp[i]+res[minNode]);
     }
     return res;
 }
--- implementation/js/InOut/baekjoon/inputReader.js
+++ implementation/js/InOut/baekjoon/inputReader.js
@@ -18,3 +18,12 @@ const reader = {
         return this.readLine().split(delimiter).map(e=>parseInt(e));
     }
 }
+
+const reader = {
+    input : require('fs').readFileSync('input.txt').toString().split("\r\n"),
+    // input : require('fs').readFileSync('/dev/stdin').toString().split("\n"),
+    index : 0,
+    read : function(){
+        return this.input[this.index++].split(" ").map(e=>parseInt(e));
+    }
+}
