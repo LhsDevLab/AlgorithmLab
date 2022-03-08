7a5fc0eb1feb801fcbf94463b89834d94e6438a7
LHS <jkhk7788@gmail.com>
2022-03-08 13:46:14 +0900

    u

--- Baekjoon/problem/js/2263.js
+++ Baekjoon/problem/js/2263.js
@@ -1,5 +1,5 @@
 const reader = {
-    input : require('fs').readFileSync('input.txt').toString().split("\r\n"),
+    input : require('fs').readFileSync('input2.txt').toString().split("\r\n"),
     // input : require('fs').readFileSync('/dev/stdin').toString().split("\n"),
     index : 0,
     read : function(){
@@ -10,19 +10,16 @@ const n = parseInt(reader.input.shift());
 const inorder = reader.read();
 const postorder = reader.read();
 const indexes = [];
-
+let res = [];
 for (let i=0; i<n; i++)
     indexes[inorder[i]] = i;
 function preOrder(start, end){
     if (start >= end || postorder.length == 0)
-        return [];
-    let root = [postorder.pop()];
-    let rear = preOrder(indexes[root]+1, end);
-    let front = preOrder(start, indexes[root]);
-    for (let e of front)
-        root.push(e);
-    for (let e of rear)
-        root.push(e);
-    return root;
+        return;
+    let root = postorder.pop();
+    preOrder(indexes[root]+1, end);
+    preOrder(start, indexes[root]);
+    res.push(root);
 }
-console.log(preOrder(0,n).join(' '));
+preOrder(0,n);
+console.log(res.reverse().join(' '));
