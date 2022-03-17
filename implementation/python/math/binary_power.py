44b3ace0fd0b754833db2c95a4a53a7889501d50
LHS <jkhk7788@gmail.com>
2022-03-17 13:19:59 +0900

    Update binary_power.py

--- implementation/python/math/binary_power.py
+++ implementation/python/math/binary_power.py
@@ -1,13 +1,15 @@
 def binary_power(base,expo):
     res = 1;
     stack = [];
-    while expo != 1:
+    while expo != 0:
         if expo%2 == 0:
             stack.append(2);
             expo /= 2;
         else:
             stack.append(1);
             expo -= 1;
+    print(stack[::-1])
     for e in stack[::-1]:
         res *= base if e == 1 else res;
+        print(res)
     return res;
