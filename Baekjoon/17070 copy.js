ad415c217d799c4c610fe0969214a337a1df6438
LHS <jkhk7788@gmail.com>
2022-03-29 15:08:29 +0900

    Create 17070 copy.js

---
+++ Baekjoon/17070 copy.js
@@ -0,0 +1,66 @@
+const reader = {
+    input : require('fs').readFileSync('input.txt').toString().split("\r\n"),
+    // input : require('fs').readFileSync('/dev/stdin').toString().split("\n"),
+    index : 0,
+    readLine : function(){
+        return this.input[this.index++];
+    },
+    readList : function(){
+        return this.readLine().split(" ").map(e=>parseInt(e));
+    }
+}
+let N = parseInt(reader.readLine());
+const dirSet = [[0,1],[1,1],[1,0]];
+const DP = Array.from({length:N},()=>
+    Array.from({length:N}, ()=>[0,0,0]));
+DP[0][1][0] = 1;
+const room = [];
+for (let i=0; i<N; i++)
+    room.push(reader.readList());
+function move(pos,dir){
+    let [a,b] = dirSet[dir];
+    return [pos[0]+a,pos[1]+b];
+}
+function check(pos){
+    try{
+        if (room[pos[0]][pos[1]] == 0)
+            return true;
+    }catch(e){}
+    return false;
+}
+function validDir(pos,dir){
+    let res = new Set();
+    if (check(move(pos,0)))
+        res.add(0);
+    if (check(move(pos,2)))
+        res.add(2);
+    if (res.size == 2 && check(move(pos,1)))
+        res.add(1);
+    if (dir == 0)
+        res.delete(2);
+    if (dir == 2)
+        res.delete(0);
+    return res;
+}
+let count = 0;
+let queue = [[[0,1],0]];
+while(queue.length != 0){
+    let next = [];
+    for (let [pos,dir] of queue){
+        // if ((pos[0] == 0 ? 1 : pos[0])+pos[1] != count+2){
+        //     next.push([pos,dir]);
+        //     continue;
+        // }
+        for (let e of validDir(pos,dir)){
+            let [r,c] = move(pos,e);
+            DP[r][c][e] += DP[pos[0]][pos[1]][dir];
+            next.push([[r,c],e]);
+        }
+    }
+    queue = next;
+    count += 1;
+}
+for(let e of DP){
+    console.log(e.map(e=>e.toString()).join(' - '));
+}
+console.log(DP[N-1][N-1].reduce((a,c)=>a+c));
