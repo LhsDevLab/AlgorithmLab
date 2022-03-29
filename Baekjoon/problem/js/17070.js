58c885b136e7bf9370df19e8dd98a73a71f40efc
LeeHanSeong7 <54903040+LeeHanSeong7@users.noreply.github.com>
2022-03-29 18:19:29 +0900

    u

--- Baekjoon/problem/js/17070.js
+++ Baekjoon/problem/js/17070.js
@@ -17,19 +17,20 @@ DP[0][1][0] = 1;
 const room = [];
 for (let i=0; i<N; i++)
     room.push(reader.readList());
+
 function move(pos,dir){
     let [a,b] = dirSet[dir];
     return [pos[0]+a,pos[1]+b];
 }
-function check(pos){
-    try{
-        if (room[pos[0]][pos[1]] == 0)
-            return true;
-    }catch(e){}
-    return false;
-}
 function validDir(pos,dir){
     let res = new Set();
+    function check(pos){
+        try{
+            if (room[pos[0]][pos[1]] == 0)
+                return true;
+        }catch(e){}
+        return false;
+    }
     if (check(move(pos,0)))
         res.add(0);
     if (check(move(pos,2)))
@@ -42,16 +43,26 @@ function validDir(pos,dir){
         res.delete(0);
     return res;
 }
-let queue = [[[0,1],0]];
-while(queue.length != 0){
-    let [pos,dir] = queue.pop();
-    for (let e of validDir(pos,dir)){
-        let next = move(pos,e)
-        DP[next[0]][next[1]][e] += DP[pos[0]][pos[1]][dir];
-        queue.push([next,e]);
+let count = 0;
+let queue = new Set(['0_1_0']);
+while(queue.size != 0){
+    let next = new Set();
+    function addQ(r,c,dir){
+        next.add([r,c,dir].join('_'));
     }
-}
-for(let e of DP){
-    console.log(e.map(e=>e.toString()).join(' - '));
+    queue = Array.from(queue).map(e=>e.split('_').map(e=>parseInt(e)));
+    for (let [r,c,dir] of queue){
+        if ((r == 0 ? 1 : r)+c != count+2){
+            addQ(r,c,dir);
+            continue;
+        }
+        for (let e of validDir([r,c],dir)){
+            let [a,b] = move([r,c],e);
+            DP[a][b][e] += DP[r][c][dir];
+            addQ(a,b,e);
+        }
+    }
+    queue = next;
+    count += 1;
 }
 console.log(DP[N-1][N-1].reduce((a,c)=>a+c));
