308eb208a948fa94103423029eb36ad7ab1f6afc
LHS <jkhk7788@gmail.com>
2022-05-05 20:26:28 +0900

    update

--- Baekjoon/7573.js
+++ Baekjoon/7573.js
@@ -1,33 +1,16 @@
-//7573
 const reader = {
     input: require('fs').readFileSync('input.txt').toString().split("\r\n"),
     // input : require('fs').readFileSync('/dev/stdin').toString().split("\n"),
     index: 0,
-    read(){
+    read() {
         return this.input[this.index++];
     }
 }
-let [N,I,M] = reader.read().split(" ").map(e => parseInt(e));
+let [N, I, M] = reader.read().split(" ").map(e => parseInt(e));
 I /= 2;
-const Fishs = {
-    set : new Set(),
-    add(v){
-        this.set.add(v);
-    },
-    has(r,c){
-        return this.set.has(r+" "+c);
-    }
-};
-for (let i=0; i<M; i++)
-    Fishs.add(reader.read());
+let count = [];
+for (let i=0; i<M; i++){
 
-const shapes = [];
-for (let i=1; i<I; i++)
-    shapes.push([i,I-i]);
-    
-let answer = 0;
-for (let [R,C] of Array.from(Fishs.set).map(e=>e.split(' ').map(e=>parseInt(e)))){
-    for (let [H,W] of shapes){
-
-    }
 }
+// 그룹의 끝점을 저장, 새로운 점으로 갱신
+// 그룹 각각을 비교해서 가능한것은 그것을 기반으로 새 그룹을 만듬
