8c51b261db033552b01cac0bf7fe9c7ea85230fb
LHS <jkhk7788@gmail.com>
2022-05-17 22:10:02 +0900

    u

--- Baekjoon/problem/python/6064.py
+++ Baekjoon/problem/python/6064.py
@@ -3,7 +3,7 @@ import math
 for _ in range(int(input())):
     M,N,x,y = map(int, input().split());
     LCM = M*N//math.gcd(M,N);
-    for n in range(x,LCM,M):
+    for n in range(x,LCM+1,M):
         if (n-y)%N == 0:
             print(n);
             break;
